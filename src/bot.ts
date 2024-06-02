5f8a0dcbc6329541057acc9b782e69e1e7179c21
Timofey Chuchkanov <timofey.chuchkanov@cmtops.dev>
2024-06-02 19:20:14 +0300

    feat(app): authenticate requests in group and private chats

--- src/bot.ts
+++ src/bot.ts
@@ -1,35 +1,106 @@
 import { logger } from './log.js';
 import { type BotConfig } from './config.js';
-import { Bot } from 'grammy';
+import { Bot, CommandContext, Context } from 'grammy';
 
-function isUserAllowed(config: BotConfig, userId: number): boolean {
-    return config.bot.acl.allow_tg_uid.includes(userId);
+type ChatType = 'group' | 'personal';
+
+export async function getBotUsername(bot: Bot): Promise<string> {
+    const facility = getBotUsername.name;
+    let botUser;
+
+    try {
+        botUser = await bot.api.getMe();
+        logger.info({ facility, message: `fetched bot username <${botUser.username}>` });
+    } catch (e: any) {
+        logger.fatal({ facility, message: e.message });
+        process.exit(1);
+    }
+
+    return botUser.username;
 }
 
-export function attachBotCallbacks(config: BotConfig, bot: Bot) {
-    bot.command('start', async ctx => {
-        const facility = '/start';
-        const userId = ctx.chatId;
-        let reply;
+export function attachBotCallbacks(botUserName: string, config: BotConfig, bot: Bot) {
+    bot.command(`start@${botUserName}`)
+        .filter(ctx => determineChatType(ctx) == 'group')
+        .filter(
+            ctx => authenticateRequest(config, ctx, `start@${botUserName}`),
+            async ctx => await ctx.reply('hi'),
+        );
 
-        if (isUserAllowed(config, userId)) {
-            reply = await ctx.reply('hi');
-            logger.info({ facility, message: `authenticated request by allowed user` });
-        } else {
-            reply = await ctx.reply('not allowed');
-            logger.error({ facility, message: `unauthenticated request by disallowed user <${ctx.chatId}>` });
+    bot.command('start')
+        .filter(ctx => determineChatType(ctx) == 'personal')
+        .filter(
+            ctx => authenticateRequest(config, ctx, 'start'),
+            async ctx => await ctx.reply('hi'),
+        );
+}
+
+function authenticateRequest(config: BotConfig, ctx: CommandContext<Context>, facility = 'filter'): boolean {
+    const chatType: ChatType = determineChatType(ctx);
+    const userId = ctx.chatId;
+    const from = ctx.from;
+    let isRequestAuthenticated = false;
+    let allowedUser!: number;
+    let disallowedUser!: number;
+
+    switch (chatType) {
+        case 'personal': {
+            const userAllowed = isUserAllowed(config, userId);
+            isRequestAuthenticated = userAllowed;
+
+            if (!userAllowed) {
+                disallowedUser = userId;
+            } else {
+                allowedUser = userId;
+            }
+
+            break;
         }
+        case 'group': {
+            if (from) {
+                const userAllowed = isUserAllowed(config, from.id);
+                isRequestAuthenticated = userAllowed;
 
-        return reply;
-    });
+                if (!userAllowed) {
+                    disallowedUser = from.id;
+                } else {
+                    allowedUser = from.id;
+                }
+            }
+
+            break;
+        }
+    }
+
+    if (isRequestAuthenticated) {
+        logger.info({
+            facility,
+            message: `authenticated request by allowed user <${allowedUser}> in chat <${ctx.chatId}>`,
+        });
+    } else {
+        logger.error({
+            facility,
+            message: `unauthenticated request by disallowed user <${disallowedUser}> in chat <${ctx.chatId}>`,
+        });
+    }
+
+    return isRequestAuthenticated;
 }
 
-export async function startBot(bot: Bot) {
-    logger.info({ facility: startBot.name, message: 'starting bot' });
+function determineChatType(ctx: CommandContext<Context>): ChatType {
+    return ctx.chatId > 0 ? 'personal' : 'group';
+}
+
+export async function startBot(bot: Bot, facility = startBot.name) {
+    logger.info({ facility, message: 'starting bot' });
     await bot.start();
 }
 
-export async function stopBot(bot: Bot) {
-    logger.info({ facility: stopBot.name, message: 'stopping bot' });
+export async function stopBot(bot: Bot, facility = stopBot.name) {
+    logger.info({ facility, message: 'stopping bot' });
     await bot.stop();
+}
+
+function isUserAllowed(config: BotConfig, userId: number): boolean {
+    return config.bot.acl.allow_tg_uid.includes(userId);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { BotConfig, loadConfig } from './config.js';
-import { startBot, stopBot, attachBotCallbacks } from './bot.js';
+import { getBotUsername, startBot, stopBot, attachBotCallbacks } from './bot.js';
 import { Bot } from 'grammy';
 
 async function reloadConfig() {
@@ -10,18 +10,19 @@ async function reloadConfig() {
         const oldBot = bot;
         bot = new Bot(config.bot.options.tg_token);
 
-        await stopBot(oldBot);
-        attachBotCallbacks(config, bot);
-        await startBot(bot);
+        await stopBot(oldBot, reloadConfig.name);
+        attachBotCallbacks(botUserName, config, bot);
+        await startBot(bot, reloadConfig.name);
     }
 }
 
 let config = (await loadConfig(true)) as BotConfig;
 let bot = new Bot(config.bot.options.tg_token);
+let botUserName = await getBotUsername(bot);
 
 process.on('SIGHUP', reloadConfig);
 
 ['SIGINT', 'SIGTERM'].forEach(signal => process.on(signal, async () => await stopBot(bot)));
 
-attachBotCallbacks(config, bot);
+attachBotCallbacks(botUserName, config, bot);
 await startBot(bot);
