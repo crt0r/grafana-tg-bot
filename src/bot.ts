056a314e85be0d006e247f40be373d9b4ac6fe86
Timofey Chuchkanov <timofey.chuchkanov@cmtops.dev>
2024-06-06 10:45:00 +0300

    feat(app): refactor bot for usage simplicity

--- src/bot.ts
+++ src/bot.ts
@@ -3,136 +3,163 @@ import { BotConfig } from './config.js';
 import { Cache } from './cache.js';
 import { Bot, CommandContext, Context } from 'grammy';
 
-const facility = 'bot';
 type ChatType = 'group' | 'personal';
+const facility = 'bot';
 
-export async function getBotUsername(bot: Bot): Promise<string> {
-    let botUser;
+export class AlertBot extends Bot {
+    private config;
+    private cache;
+    private userName: string | null = null;
 
-    try {
-        botUser = await bot.api.getMe();
-        logger.info({ facility, message: `fetched bot username <${botUser.username}>` });
-    } catch (e: any) {
-        logger.fatal({ facility, message: e.message });
-        process.exit(1);
+    constructor(config: BotConfig, cache: Cache) {
+        super(config.bot.options.tg_token);
+
+        this.config = config;
+        this.cache = cache;
     }
 
-    return botUser.username;
-}
+    async start() {
+        this.userName = await this.getUsername();
+        this.attachMiddlewares();
+        logger.info({ facility, message: 'starting bot' });
+        await super.start();
+    }
 
-export function attachBotMiddlewares(botUserName: string, config: BotConfig, bot: Bot, cache: Cache) {
-    const group = (ctx: CommandContext<Context>) => determineChatType(ctx) == 'group';
-    const personal = (ctx: CommandContext<Context>) => determineChatType(ctx) == 'personal';
-    const authRequest = (ctx: CommandContext<Context>) => authenticateRequest(config, ctx);
+    async stop() {
+        await super.stop();
+        logger.info({ facility, message: 'bot stopped' });
+    }
 
-    bot.command(`start@${botUserName}`).filter(group).filter(authRequest, subscribeChat(cache, true));
-    bot.command(`stop@${botUserName}`).filter(group).filter(authRequest, unsubscribeChat(cache, true));
+    private async getUsername(): Promise<string> {
+        let botUser;
 
-    bot.command('start').filter(personal).filter(authRequest, subscribeChat(cache));
-    bot.command('stop').filter(personal).filter(authRequest, unsubscribeChat(cache));
-}
-
-function subscribeChat(cache: Cache, replyTo = false) {
-    return async (ctx: CommandContext<Context>) => {
-        const isAlreadySubscribed = await cache.isChatSubscribedToAlerts(ctx.chatId);
-
-        if (!isAlreadySubscribed) {
-            const reply = await ctx.reply('This chat is now subscribed to Grafana alerts.', withReplyTo(ctx, replyTo));
-            await cache.addSubscriberChat(ctx.chatId);
-            return reply;
-        } else {
-            return await ctx.reply('This chat is already subscribed to Grafana alerts.', withReplyTo(ctx, replyTo));
+        try {
+            botUser = await this.api.getMe();
+            logger.info({ facility, message: `fetched bot username <${botUser.username}>` });
+        } catch (e: any) {
+            logger.fatal({ facility, message: e.message });
+            process.exit(1);
         }
-    };
-}
 
-function unsubscribeChat(cache: Cache, replyTo = false) {
-    return async (ctx: CommandContext<Context>) => {
-        const isAlreadySubscribed = await cache.isChatSubscribedToAlerts(ctx.chatId);
+        return botUser.username;
+    }
 
-        if (isAlreadySubscribed) {
-            const reply = await ctx.reply(
-                'This chat will no longer receive Grafana alerts.',
-                withReplyTo(ctx, replyTo),
-            );
-            await cache.delSubscriberChat(ctx.chatId);
-            return reply;
-        } else {
-            return ctx.reply('This chat is not subscribed to Grafana alerts yet.', withReplyTo(ctx, replyTo));
-        }
-    };
-}
+    private attachMiddlewares() {
+        const filters = {
+            groupChat: (ctx: CommandContext<Context>) => this.determineChatType(ctx) == 'group',
+            personalChat: (ctx: CommandContext<Context>) => this.determineChatType(ctx) == 'personal',
+            authRequest: (ctx: CommandContext<Context>) => this.authenticateRequest(this.config, ctx),
+        };
 
-function withReplyTo(ctx: CommandContext<Context>, replyTo: boolean) {
-    return replyTo ? { reply_parameters: { message_id: ctx.msg.message_id } } : {};
-}
+        this.command(`start@${this.userName}`)
+            .filter(filters.groupChat)
+            .filter(filters.authRequest, this.subscribeChat(true));
+        this.command(`stop@${this.userName}`)
+            .filter(filters.groupChat)
+            .filter(filters.authRequest, this.unsubscribeChat(true));
 
-function authenticateRequest(config: BotConfig, ctx: CommandContext<Context>): boolean {
-    const chatType: ChatType = determineChatType(ctx);
-    const userId = ctx.chatId;
-    const from = ctx.from;
-    let isRequestAuthenticated = false;
-    let allowedUser!: number;
-    let disallowedUser!: number;
+        this.command('start').filter(filters.personalChat).filter(filters.authRequest, this.subscribeChat());
+        this.command('stop').filter(filters.personalChat).filter(filters.authRequest, this.unsubscribeChat());
+    }
 
-    switch (chatType) {
-        case 'personal': {
-            const userAllowed = isUserAllowed(config, userId);
-            isRequestAuthenticated = userAllowed;
+    private subscribeChat(replyTo = false) {
+        return async (ctx: CommandContext<Context>) => {
+            const isAlreadySubscribed = await this.cache.isChatSubscribedToAlerts(ctx.chatId);
 
-            if (!userAllowed) {
-                disallowedUser = userId;
+            if (!isAlreadySubscribed) {
+                const reply = await ctx.reply(
+                    'This chat is now subscribed to Grafana alerts.',
+                    this.withReplyTo(ctx, replyTo),
+                );
+                await this.cache.addSubscriberChat(ctx.chatId);
+                return reply;
             } else {
-                allowedUser = userId;
+                return await ctx.reply(
+                    'This chat is already subscribed to Grafana alerts.',
+                    this.withReplyTo(ctx, replyTo),
+                );
             }
+        };
+    }
 
-            break;
-        }
-        case 'group': {
-            if (from) {
-                const userAllowed = isUserAllowed(config, from.id);
+    private unsubscribeChat(replyTo = false) {
+        return async (ctx: CommandContext<Context>) => {
+            const isAlreadySubscribed = await this.cache.isChatSubscribedToAlerts(ctx.chatId);
+
+            if (isAlreadySubscribed) {
+                const reply = await ctx.reply(
+                    'This chat will no longer receive Grafana alerts.',
+                    this.withReplyTo(ctx, replyTo),
+                );
+                await this.cache.delSubscriberChat(ctx.chatId);
+                return reply;
+            } else {
+                return ctx.reply('This chat is not subscribed to Grafana alerts yet.', this.withReplyTo(ctx, replyTo));
+            }
+        };
+    }
+
+    private withReplyTo(ctx: CommandContext<Context>, replyTo: boolean) {
+        return replyTo ? { reply_parameters: { message_id: ctx.msg.message_id } } : {};
+    }
+
+    private authenticateRequest(config: BotConfig, ctx: CommandContext<Context>): boolean {
+        const chatType: ChatType = this.determineChatType(ctx);
+        const userId = ctx.chatId;
+        const from = ctx.from;
+        let isRequestAuthenticated = false;
+        let allowedUser!: number;
+        let disallowedUser!: number;
+
+        switch (chatType) {
+            case 'personal': {
+                const userAllowed = this.isUserAllowed(userId);
                 isRequestAuthenticated = userAllowed;
 
                 if (!userAllowed) {
-                    disallowedUser = from.id;
+                    disallowedUser = userId;
                 } else {
-                    allowedUser = from.id;
+                    allowedUser = userId;
                 }
+
+                break;
             }
+            case 'group': {
+                if (from) {
+                    const userAllowed = this.isUserAllowed(from.id);
+                    isRequestAuthenticated = userAllowed;
 
-            break;
+                    if (!userAllowed) {
+                        disallowedUser = from.id;
+                    } else {
+                        allowedUser = from.id;
+                    }
+                }
+
+                break;
+            }
         }
+
+        if (isRequestAuthenticated) {
+            logger.info({
+                facility,
+                message: `authenticated request <${ctx.message?.text}> by allowed user <${allowedUser}> in chat <${ctx.chatId}>`,
+            });
+        } else {
+            logger.error({
+                facility,
+                message: `unauthenticated request <${ctx.message?.text}> by disallowed user <${disallowedUser}> in chat <${ctx.chatId}>`,
+            });
+        }
+
+        return isRequestAuthenticated;
     }
 
-    if (isRequestAuthenticated) {
-        logger.info({
-            facility,
-            message: `authenticated request <${ctx.message?.text}> by allowed user <${allowedUser}> in chat <${ctx.chatId}>`,
-        });
-    } else {
-        logger.error({
-            facility,
-            message: `unauthenticated request <${ctx.message?.text}> by disallowed user <${disallowedUser}> in chat <${ctx.chatId}>`,
-        });
+    private determineChatType(ctx: CommandContext<Context>): ChatType {
+        return ctx.chatId > 0 ? 'personal' : 'group';
     }
 
-    return isRequestAuthenticated;
-}
-
-function determineChatType(ctx: CommandContext<Context>): ChatType {
-    return ctx.chatId > 0 ? 'personal' : 'group';
-}
-
-export async function startBot(bot: Bot) {
-    logger.info({ facility, message: 'starting bot' });
-    await bot.start();
-}
-
-export async function stopBot(bot: Bot) {
-    logger.info({ facility, message: 'stopping bot' });
-    await bot.stop();
-}
-
-function isUserAllowed(config: BotConfig, userId: number): boolean {
-    return config.bot.acl.allow_tg_uid.includes(userId);
+    private isUserAllowed(userId: number): boolean {
+        return this.config.bot.acl.allow_tg_uid.includes(userId);
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { BotConfig, loadConfig } from './config.js';
-import { getBotUsername, startBot, stopBot, attachBotMiddlewares } from './bot.js';
+import { AlertBot } from './bot.js';
 import { Cache } from './cache.js';
 import { WebhookServer } from './webhook.js';
-import { Bot } from 'grammy';
 
 async function reloadConfig() {
     const newConfig = await loadConfig(false);
@@ -13,38 +12,35 @@ async function reloadConfig() {
         const oldCache = cache;
         const oldWebHook = webHook;
 
-        bot = new Bot(config.bot.options.tg_token);
         cache = new Cache(config);
+        bot = new AlertBot(config, cache);
         webHook = new WebhookServer(config);
 
         oldWebHook.close();
-        await stopBot(oldBot);
+        await oldBot.stop();
         await oldCache.quit();
 
-        attachBotMiddlewares(botUserName, config, bot, cache);
         await cache.connect();
-        startBot(bot);
+        bot.start();
         webHook.listen();
     }
 }
 
 let config = (await loadConfig(true)) as BotConfig;
 let cache = new Cache(config);
-let bot = new Bot(config.bot.options.tg_token);
+let bot = new AlertBot(config, cache);
 let webHook = new WebhookServer(config);
-let botUserName = await getBotUsername(bot);
 
 process.on('SIGHUP', reloadConfig);
 
 ['SIGINT', 'SIGTERM'].forEach(signal =>
     process.on(signal, async () => {
         webHook.close();
-        await stopBot(bot);
+        await bot.stop();
         await cache.quit();
     }),
 );
 
-attachBotMiddlewares(botUserName, config, bot, cache);
 await cache.connect();
-startBot(bot);
+bot.start();
 webHook.listen();
